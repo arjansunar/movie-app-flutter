870d3be95c20977025cee769b82fb3baa10a3587
arjansunar <arjan.gahatrajsunar@gmail.com>
2022-07-19 19:55:38 +0545

    firebase auth

---
+++ lib/src/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'screen/login.dart';
+
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(  
+      title: 'Login App',
+      theme: ThemeData(  
+        accentColor: Colors.orange,
+        primarySwatch: Colors.blue
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
